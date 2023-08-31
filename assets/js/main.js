ae5ef75c78f8183020972cf410a18734798e9bd6
ufuksonmezuA <ufukasonmez@gmail.com>
2023-08-31 20:06:17 +0300

    bazı değişiklikler yapıldı ( favicon, font, manuel tema değişimi butonu kaldırıldı onun yerine kullanıcı sistemine göre otomatik tema değişimi eklendi)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,145 +1,135 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById("nav-menu"),
-        navToggle = document.getElementById("nav-toggle"),
-        navClose = document.getElementById("nav-close")
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener("click", () =>{
-        navMenu.classList.add("show-menu")
-    })
+if (navToggle) {
+        navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener("click", () =>{
-        navMenu.classList.remove("show-menu")
-    })
+if (navClose) {
+        navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
 }
 
 /*=============== SHOW MODALCART ===============*/
 const modalCart = document.getElementById("modal-cart"),
-        modalButton = document.getElementById("modal-cart-button"),
-        modalClose = document.getElementById("modal-cart-close")
+    modalButton = document.getElementById("modal-cart-button"),
+    modalClose = document.getElementById("modal-cart-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(modalButton){
-    modalButton.addEventListener("click", () =>{
-        modalCart.classList.add("modal__show")
-    })
+if (modalButton) {
+        modalButton.addEventListener("click", () => {
+        modalCart.classList.add("modal__show");
+    });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(modalClose){
-    modalClose.addEventListener("click", () =>{
-        modalCart.classList.remove("modal__show")
-    })
+if (modalClose) {
+        modalClose.addEventListener("click", () => {
+        modalCart.classList.remove("modal__show");
+    });
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll(".nav__link")
+const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = () =>{
-    const navMenu = document.getElementById("nav-menu")
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove("show-menu")
-}
-navLink.forEach(n => n.addEventListener("click", linkAction))
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
-    const header = document.getElementById("header")
+const scrollHeader = () => {
+    const header = document.getElementById("header");
     // When the scroll is greater than 72 viewport height, add the scroll-header class to the header tag
-    this.scrollY >= 72 ? header.classList.add("scroll-header") 
-                        : header.classList.remove("scroll-header")
-}
-window.addEventListener("scroll", scrollHeader)
+    this.scrollY >= 72
+        ? header.classList.add("scroll-header")
+        : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll("section[id]")
-    
-const scrollActive = () =>{
-    const scrollDown = window.scrollY
+const sections = document.querySelectorAll("section[id]");
 
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-                sectionTop = current.offsetTop - 72,
-                sectionId = current.getAttribute("id"),
-                sectionsClass = document.querySelector(".nav__menu a[href*=" + sectionId + "]")
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
 
-		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-			sectionsClass.classList.add("active-link")
-		}else{
-			sectionsClass.classList.remove("active-link")
-		}                                                    
-	})
-}
-window.addEventListener("scroll", scrollActive)
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 72,
+            sectionId = current.getAttribute("id"),
+            sectionsClass = document.querySelector(
+                ".nav__menu a[href*=" + sectionId + "]"
+            );
 
-/*=============== SHOW SCROLL UP ===============*/ 
-const scrollUp = () =>{
-	const scrollUp = document.getElementById("scroll-up")
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add("active-link");
+        } else {
+            sectionsClass.classList.remove("active-link");
+        }
+    });
+};
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById("scroll-up");
     // When the scroll is higher than 384 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 384 ? scrollUp.classList.add("show-scroll")
-						: scrollUp.classList.remove("show-scroll")
-}
-window.addEventListener("scroll", scrollUp)
+    this.scrollY >= 384
+        ? scrollUp.classList.add("show-scroll")
+        : scrollUp.classList.remove("show-scroll");
+    };
+window.addEventListener("scroll", scrollUp);
 
-/*=============== DARK LIGHT THEME ===============*/ 
-const themeButton = document.getElementById("theme-button")
-const darkTheme = "dark-theme"
-const iconTheme = "ri-sun-line"
+/*=============== DARK LIGHT THEME ===============
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem("selected-theme")
-const selectedIcon = localStorage.getItem("selected-icon")
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? "dark" : "light"
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line"
+const getCurrentTheme = () =>
+    document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+    themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme)
-    themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](iconTheme)
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+    );
+    themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+    );
 }
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener("click", () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem("selected-theme", getCurrentTheme())
-    localStorage.setItem("selected-icon", getCurrentIcon())
-})
+  // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
+});*/
 
-/*=============== Device Dark Mode is On or Off ===============*/ 
-const darkThemeQuery = window.matchMedia("(prefers-color-scheme: dark)");
-const detectDarkMode = event =>{
-    if(event.matches)
-    {
-        document.body.classList.add(darkTheme);
-        console.log("Device Dark Mode is On");
-    }
-    else
-    {
-        document.body.classList.remove(darkTheme);
-        console.log("Device Dark Mode is Off");
-    }
-}
-detectDarkMode(darkThemeQuery);
-darkThemeQuery.addEventListener("change", e => detectDarkMode(e));
-
-/*=============== NAVIGATOR LANGUAGE ===============*/
-const userLanguage = navigator.language;
-console.log("Users language", userLanguage);
-
-/*=============== NAVIGATOR ONLINE ===============*/
-const isUserOnline = navigator.onLine;
-const statusMessage = console.log(isUserOnline ? "User is online" : "User is not online");
+/*=============== DYNAMIC YEAR ===============*/
+// let year = document.getElementById("year");
+// let dynamicYear = new Date().getFullYear();
+// year.innerHTML = dynamicYear;
